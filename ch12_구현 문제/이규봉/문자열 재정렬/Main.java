90cf0dbe6555b860e21cd201d14ae1738cfe0917
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-04-07 14:38:45 +0900

    이규봉 / 문자열 재정렬 완료

--- ch12_구현 문제/이규봉/문자열 재정렬/Main.java
+++ ch12_구현 문제/이규봉/문자열 재정렬/Main.java
@@ -1,33 +1,32 @@
-package boj_18406_럭키_스트레이트;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        //왼쪽 합
-        int left = 0;
-        //오른쪽 합
-        int right = 0;
-        //글자수 반
-        int mid = input.length() / 2;
+        //알파벳 대문자를 담을 리스트
+        List<Character> charList = new LinkedList<>();
 
-        //각 자릿수 더하기
-        for (int i = 0; i < mid; i++) {
-            left += input.charAt(i) - '0';
-            right += input.charAt(mid + i) - '0';
+        //숫자 합을 계산할 변수
+        int sum = 0;
+
+        //숫자라면 sum에 더하고 대문자라면 리스트에 담기
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                sum += input.charAt(i) - '0';
+            } else {
+                charList.add(input.charAt(i));
+            }
         }
 
-        //같다면 LUCKY 출력
-        if (left == right) {
-            System.out.println("LUCKY");
-        } else {
-            System.out.println("READY");
-        }
-
+        //알파벳 순으로 이어서 출력
+        charList.stream().sorted().forEach(System.out::print);
+        //숫자 합 출력
+        System.out.println(sum);
     }
 }
