6ecc297b2ff7670f5c27d4ee21c58462d2bf9caa
yoon <dudwls143@gmail.com>
2022-04-08 15:01:14 +0900

    윤영진 / 뱀 완성

--- ch12_구현 문제/윤영진/뱀/src/Main.java
+++ ch12_구현 문제/윤영진/뱀/src/Main.java
@@ -5,102 +5,116 @@ import java.util.*;
 
 public class Main {
 
-    static int N; // 보드 크기
-    static int K; // 사과 개수
-    static int L; // 뱀의 방향 변환 횟수
-    static int time = 0; // 게임 시간
-
-    static int[][] board;
-
-    static List<Point> snake = new LinkedList<>();
-
-    static int idx = 0; // 처음 방향 오른쪽
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static Map<Integer, String> dir = new HashMap<>(); // 뱀의 방향 정보
-
     public static class Point {
         int x;
         int y;
-
-        public Point() {
-        }
-
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
+    static int N;
+    static int K;
+    static int L;
+    static int idx = 0;
+
+    static Point[] direction = new Point[]{new Point(0, 1), new Point(1, 0), new Point(0, -1), new Point(-1, 0)};
+
+    static int[][] board;
+    static List<Point> snake = new LinkedList<>();
+    static Map<Integer, String> timeTable = new HashMap<>();
+
+    static int time = 0;
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
+        board = new int[N + 1][N + 1];
+
         K = Integer.parseInt(br.readLine());
 
-        board = new int[N + 1][N + 1];
+        /**
+         * 사과가 존재하는 board 초기화
+         */
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
             board[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
         }
 
         L = Integer.parseInt(br.readLine());
+        /**
+         * timeTable 초기화
+         */
         for (int i = 0; i < L; i++) {
             st = new StringTokenizer(br.readLine());
-            dir.put(Integer.parseInt(st.nextToken()), st.nextToken());
+            timeTable.put(Integer.parseInt(st.nextToken()), st.nextToken());
         }
+
+        /**
+         * snake (1,1) 시작
+         */
         snake.add(new Point(1, 1));
 
-        Point nextPoint;
         Point curPoint = new Point(1, 1);
 
         while (true) {
             time ++;
+            Point nextPoint = new Point(curPoint.x + direction[idx].x, curPoint.y + direction[idx].y);
 
-            nextPoint = new Point();
-            nextPoint.x = curPoint.x + dx[idx];
-            nextPoint.y = curPoint.y + dy[idx];
+            if(check(nextPoint)) break;
 
-            if(End(nextPoint)) break;
-
+            /**
+             * 사과 유무 확인 후 뱀 길이 조정
+             */
             if (board[nextPoint.x][nextPoint.y] == 1) {
-                board[nextPoint.x][nextPoint.y] = 0;
                 snake.add(nextPoint);
+                board[nextPoint.x][nextPoint.y] = 0;
             }else {
                 snake.add(nextPoint);
                 snake.remove(0);
             }
 
+
+            if(timeTable.containsKey(time)) {
+                String dir = timeTable.get(time);
+                if (dir.equals("D")) {
+                    idx ++;
+                    if (idx == 4){
+                        idx = 0;
+                    }
+                } else {
+                    idx --;
+                    if(idx == -1) {
+                        idx = 3;
+                    }
+                }
+            }
             curPoint = nextPoint;
 
-            if(dir.containsKey(time)) {
-                // D(오른쪽)가 다오면 index++
-                if(dir.get(time).equals("D")) {
-                    idx++;
-                    if(idx == 4)
-                        idx = 0;
-                }
-                // L(왼쪽)이 나오면 index--
-                if(dir.get(time).equals("L")) {
-                    idx--;
-                    if(idx == -1)
-                        idx = 3;
-                }
-            }
         }
         System.out.println(time);
+
     }
 
-    private static boolean End(Point nextPoint) {
+    private static boolean check(Point nextPoint) {
 
-        if(nextPoint.x < 1 || nextPoint.y < 1 || nextPoint.x >= N +1 || nextPoint.y >= N+1)
+        /**
+         * 벽에 부딪히는 경우
+         */
+        if (nextPoint.x <= 0 || nextPoint.y <= 0 || nextPoint.x > N || nextPoint.y > N)
             return true;
 
+        /**
+         * 뱀 본인한테 부딪히는 경우
+         */
         for (int i = 0; i < snake.size(); i++) {
-            if (nextPoint.x == snake.get(i).x && nextPoint.y == snake.get(i).y) {
+            if(nextPoint.x == snake.get(i).x && nextPoint.y == snake.get(i).y)
                 return true;
-            }
         }
         return false;
+
     }
 }
