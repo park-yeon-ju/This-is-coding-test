4859f32f9a0eb2bb52485255bbe77e0260993e33
yoon <dudwls143@gmail.com>
2022-04-12 16:34:22 +0900

    윤영진 / 문자열 재정렬 수정

--- ch12_구현 문제/윤영진/untitled/src/Main.java
+++ ch12_구현 문제/윤영진/untitled/src/Main.java
@@ -1,29 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String s = br.readLine();
 
-        List<Character> characters = new ArrayList<>();
-
         int sum = 0;
+        List<Character> characters = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
             char temp = s.charAt(i);
+            // 숫자
             if (temp < 65) {
                 sum += Integer.parseInt(String.valueOf(temp));
             }else {
                 characters.add(temp);
             }
         }
+
         Collections.sort(characters);
-        for (Character c : characters) {
-            System.out.print(c);
+        for (Character character : characters) {
+            System.out.print(character);
         }
         System.out.print(sum != 0 ? sum : "");
+
+
+
     }
 }
